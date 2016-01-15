4bf149899e0ab1c8a8bc0a941ee85f8161ba2933
namhla1986 <mankunen@gmail.com>
2016-01-15 10:56:02 +0200

    Create inc-cms-navbar-main.php

---
+++ cms/inc-cms-navbar-main.php
@@ -0,0 +1,24 @@
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+
+</head>
+
+<body>
+
+<div id="navbar_container" class="navbarmain">
+
+<a href="cms-homepage.php" title="CMS Home">Home</a>
+&nbsp;&nbsp;<span style="font-size: 20px; color: #ccc;">|</span>&nbsp;&nbsp;
+<a href="signout.php" title="Sign out">Sign out</a>
+
+
+</div>
+
+</body>
+</html>
+
+

479afd0d308479ff861689ce23b89eafac88997d
namhla1986 <mankunen@gmail.com>
2016-01-15 10:45:44 +0200

    Create cms-homepage.php

---
+++ cms/cms-homepage.php
@@ -0,0 +1,50 @@
+<?php require("inc-cms-pre-doctype.php"); ?>
+<!DOCTYPE HTML>
+<html>
+
+<head>
+<?php require("inc-cms-head-content.php"); ?>
+</head>
+
+<body>
+
+<div id="main_container">
+
+<div id="branding_bar">
+<?php require("inc-cms-branding-bar.php"); ?>
+</div>
+
+<div id="body_column_left_container">
+
+    <div id="body_column_left">
+        <?php require("inc-cms-accordion_menu.php"); ?>
+    </div>
+    
+</div>
+
+<div id="body_column_right_container">
+    
+    <div id="body_column_right">
+	<img src="../sources/icons/flower.png" id="flower">
+<div class="clear_float"></div>
+        <table cellspacing="0" class="tbldatadisplay">
+        <tr>
+        <td style="text-align:center;"><h1><strong>Tekiah Foundation</strong></h1></td>
+        </tr>
+        <tr>
+        <td style="text-align:center;"><h3>Manage your web content</h3></td>
+        </tr>
+        <tr>
+        <td>&nbsp;</td>
+        </tr>
+        </table>
+    </div>
+        
+</div>
+
+<div class="clearfloat_both"></div>
+
+</div>
+
+</body>
+</html>

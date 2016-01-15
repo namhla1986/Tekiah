bf40207ea339f5d142f9a7e8136bc69335f527ad
namhla1986 <mankunen@gmail.com>
2016-01-15 11:00:15 +0200

    Create news-delete.php

---
+++ cms/news-delete.php
@@ -0,0 +1,42 @@
+<?php 
+$vmysqlserver = 'localhost';
+$vconnusername = 'brian';
+$vconnpassword = 'brian';
+$vconndb = 'dbtekiah';
+
+// CONNECT TO MYSQL SERVER
+
+$vconntekiah = mysqli_connect($vmysqlserver, $vconnusername, $vconnpassword, $vconndb);
+if (!$vconntekiah){
+
+  die('Could not connect: ' . mysqli_error());
+}
+
+else{
+	
+$vid = $_POST['kid'];
+
+$sql = "DELETE FROM tblnews WHERE nid = $vid";
+
+		mysqli_select_db($vconntekiah, $vconndb);
+
+$retval = mysqli_query($vconntekiah, $sql);
+}
+
+if(! $retval )
+{
+  die('Could not delete data: ' . mysqli_error($vconntekiah));
+}
+
+	else
+		
+		header('Location: ' . $_SESSION['svabsuri']  . 'news-view.php?');
+		print "Deleted data successfully\n";
+
+		exit();
+
+
+mysqli_close($vconntekiah);
+
+
+?>

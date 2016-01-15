3635a6f3c97d3d1a772351b284d509d06c55eda0
namhla1986 <mankunen@gmail.com>
2016-01-15 10:26:26 +0200

    Create inc-navbar-mobile.php

---
+++ inc-navbar-mobile.php
@@ -0,0 +1,12 @@
+<nav id="mobile_nav">
+    <!-- MOBILE NAVBAR -->
+    <ul id="hamburgermenu">
+        <li>Home</li>
+        <li>Projects</li>
+        <li>News</li>
+        <li>Events</li>
+        <li>Support</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+</nav>
